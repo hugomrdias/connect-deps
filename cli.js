2a809c5d0ffb20cb6b8899097a3e9ae3c216c703
Hugo Dias <hugomrdias@gmail.com>
2019-07-11 15:38:31 +0100

    feat: initial commit

---
+++ cli.js
@@ -0,0 +1,194 @@
+#!/usr/bin/env node
+/* eslint-disable guard-for-in */
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const meow = require('meow');
+const execa = require('execa');
+const del = require('del');
+const chokidar = require('chokidar');
+const readPkgUp = require('read-pkg-up');
+const Conf = require('conf');
+
+const cwd = fs.realpathSync(process.cwd());
+const { package: pkg } = readPkgUp.sync({ cwd });
+const conf = new Conf({
+    cwd,
+    configName: '.connect-deps'
+});
+
+const cli = meow(`
+Usage
+    $ connect-deps [cmd]
+
+Commands
+    link /relative/path  Relative path from cwd to dependency to connect.
+    connect              Connect a linked dependency.
+    reset                Reset everything and clean up.
+
+Options
+    --help               Show help.
+    --watch, -w          Watch for changes, works with the 'connect' command only.
+
+Examples
+    $ connect-deps link ../dep-folder
+    $ connect-deps connect -w
+`, {
+    flags: {
+        watch: {
+            type: 'boolean',
+            alias: 'w'
+        }
+    }
+});
+
+const cmd = cli.input[0];
+
+if (cmd === 'link') {
+    link();
+}
+
+if (cmd === 'connect') {
+    run();
+}
+
+if (cmd === 'reset') {
+    reset();
+}
+
+function link() {
+    const connectedPath = path.resolve(cwd, cli.input[1]);
+    const connectedPkg = readPkgUp.sync({ cwd: path.resolve(cwd, cli.input[1]) });
+
+    if (connectedPkg) {
+        let version;
+
+        for (const prop in pkg.dependencies) {
+            if (prop === connectedPkg.package.name) {
+                version = pkg.dependencies[prop];
+                conf.set(connectedPkg.package.name, {
+                    name: connectedPkg.package.name,
+                    path: connectedPath,
+                    version: connectedPkg.package.version,
+                    watch: '**/*',
+                    snapshot: {
+                        version,
+                        type: 'normal'
+                    }
+                });
+            }
+        }
+
+        if (!version) {
+            for (const prop in pkg.devDependencies) {
+                if (prop === connectedPkg.package.name) {
+                    version = pkg.devDependencies[prop];
+                    conf.set(connectedPkg.package.name, {
+                        name: connectedPkg.package.name,
+                        path: connectedPath,
+                        version: connectedPkg.package.version,
+                        watch: '**/*',
+                        snapshot: {
+                            version,
+                            type: 'dev'
+                        }
+                    });
+                }
+            }
+        }
+
+        if (version) {
+            packInstall(conf.get(connectedPkg.package.name));
+        }
+    } else {
+        console.error(`Error: can't find ${connectedPath}`);
+    }
+}
+
+async function run() {
+    const deps = conf.store;
+    const all = [];
+
+    for (const key in deps) {
+        const value = deps[key];
+
+        if (cli.flags.watch && value.watch) {
+            chokidar
+                .watch(value.watch, {
+                    ignored: /node_modules|\.git/,
+                    cwd: value.path,
+                    // awaitWriteFinish: true,
+                    ignoreInitial: true
+                })
+                .on('ready', () => {
+                    console.log(`Watching in ${value.path} for ${value.watch}`);
+                })
+                .on('all', async () => {
+                    await packInstall(deps[key]);
+                })
+                .on('error', err => console.error('error watching: ', err));
+        } else {
+            all.push(packInstall(deps[key]));
+        }
+    }
+
+    try {
+        await Promise.all(all);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function reset() {
+    const deps = conf.store;
+    const all = [];
+
+    for (const key in deps) {
+        const value = deps[key];
+
+        console.log(`Resetting ${key}...`);
+        all.push(execa('yarn', [
+            'add',
+            `${value.name}@${value.snapshot.version}`,
+            value.snapshot.type === 'dev' ? '--dev' : ''
+        ], { cwd }));
+        all.push(del(path.join(cwd, '.connect-deps.json')));
+        all.push(del(path.join(value.path, 'node_modules/.connect-deps-cache'), { force: true }));
+    }
+
+    try {
+        await Promise.all(all);
+        console.log('Resetting done.');
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function packInstall(config) {
+    if (config.running) {
+        console.log('Connect is already running, skipped.');
+
+        return;
+    }
+
+    conf.set(config.name, Object.assign(config, { running: true }));
+    console.log(`Connecting ${config.name}...`);
+    const name = `./node_modules/.connect-deps-cache/${config.name}-${config.version}-${Date.now()}.tgz`;
+
+    fs.mkdirSync(path.join(config.path, 'node_modules/.connect-deps-cache'), { recursive: true });
+    try {
+        await execa('yarn', ['pack', '--filename', name], { cwd: config.path });
+        await execa('yarn', [
+            'add',
+            'file:' + path.join(config.path, name),
+            config.snapshot.type === 'dev' ? '--dev' : ''
+        ], { cwd });
+        console.log(`Connecting ${config.name} done.`);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        conf.set(config.name, Object.assign(config, { running: false }));
+    }
+}
---
+++ readme.md
@@ -0,0 +1,49 @@
+# connect-deps [![NPM Version](https://img.shields.io/npm/v/connect-deps.svg)](https://www.npmjs.com/package/connect-deps) [![NPM Downloads](https://img.shields.io/npm/dt/connect-deps.svg)](https://www.npmjs.com/package/connect-deps) [![NPM License](https://img.shields.io/npm/l/connect-deps.svg)](https://www.npmjs.com/package/connect-deps) [![Build Status](https://travis-ci.org/hugomrdias/connect-deps.svg?branch=master)](https://travis-ci.org/hugomrdias/connect-deps) [![codecov](https://codecov.io/gh/hugomrdias/connect-deps/badge.svg?branch=master)](https://codecov.io/gh/hugomrdias/connect-deps?branch=master)
+
+> This is a CLI tool that intends to be a better yarn link
+
+## Usage
+
+```bash
+# first link the dependency from your current repo
+connect-deps link ../package-linkded
+# after you can `connect` to stay linked
+connect-deps connect
+# or use watch mode to listen to changes and update the current repo
+connect-deps connect -w
+# after you done with coding run `reset` to cleanup and go back to previous versions
+connect-deps reset
+
+```
+
+
+## CLI
+
+```
+$ npm install --global connect-deps
+```
+
+```
+$ connect-deps --help
+
+Usage
+    $ connect-deps [cmd]
+
+Commands
+    link /relative/path  Relative path from cwd to dependency to connect.
+    connect              Connect a linked dependency.
+    reset                Reset everything and clean up.
+
+Options
+    --help               Show help.
+    --watch, -w          Watch for changes, works with the 'connect' command only.
+
+Examples
+    $ connect-deps link ../dep-folder
+    $ connect-deps connect -w
+```
+
+
+## License
+
+MIT © [Hugo Dias](http://hugodias.me)
